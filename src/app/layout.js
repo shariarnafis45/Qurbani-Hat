2dbe031f1c2c50dbc6c3c130f0e3dfb57a91b8fa
shariarnafis45 <nafisshahworkmail@gmail.com>
2026-05-04 17:46:57 +0600

    setup next , install daisy ui , reacts icons , setup poppins , playFair font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,16 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import {  Playfair_Display, Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+
+export const playfair = Playfair_Display({
+  subsets: ['latin'],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+export const poppins = Poppins({
+  subsets : ["latin"],
+  weight : ['400', '500', '600', '700', '800']
+})
+
 
 export const metadata = {
   title: "Create Next App",
@@ -20,7 +21,8 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      data-theme="light"
+      className={`${poppins.className} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
